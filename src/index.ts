9742ae36a6608356a6fb65ba9f1a4f8fb9186d76
Warren <warren@ultiverse.io>
2022-05-23 19:36:21 +0800

    Support CreateCall 1.3.0

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,15 @@ import { SafeProviderAdapter } from "./adapter"
 
 export const setupSafeDeployer = (signer: Wallet, safe: string, serivceUrl?: string) => {
     extendEnvironment((env) => {
+        const { chainId } = env.network.config;
+        if (!chainId) {
+          throw new Error('The chainId was required in hardhat network config');
+        }
         env.network.provider = new SafeProviderAdapter(
           env.network.provider,
           signer.connect(env.ethers.provider),
           safe,
+          chainId, 
           serivceUrl
         )
     })
